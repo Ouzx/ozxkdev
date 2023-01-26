def0227a5636e0295fcf903f8b4988e4a3dad784
Ouzx <oguzhankandakoglu@gmail.com>
2023-01-26 12:42:31 +0300

    fonts added

---
+++ src/app/fonts.ts
@@ -0,0 +1,11 @@
+import { Montserrat, Raleway } from "@next/font/google";
+
+export const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-montserrat",
+});
+
+export const raleway = Raleway({
+  subsets: ["latin"],
+  variable: "--font-raleway",
+});
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,13 @@
-import './globals.css'
+import { montserrat, raleway } from "./fonts";
+import "./globals.scss";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${montserrat.variable} ${raleway.variable}`}>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
@@ -14,5 +15,5 @@ export default function RootLayout({
       <head />
       <body>{children}</body>
     </html>
-  )
+  );
 }
