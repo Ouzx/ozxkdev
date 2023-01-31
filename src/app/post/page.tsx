1abdc56881d084bb2fff5e2c17bde3b0cabaa8a6
Ouzx <oguzhankandakoglu@gmail.com>
2023-01-31 15:44:06 +0300

    post page

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
-
+import styles from "./page.module.scss";
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div className={styles.post}>
+      <h1>The midday swim</h1>
+    </div>
+  );
 };
 
 export default page;
