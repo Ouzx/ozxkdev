786e27f3bf1a218dfd2f7031cf36fe7730f91f9b
Ouzx <oguzhankandakoglu@gmail.com>
2023-02-02 01:03:29 +0300

    width-maker applied all components

--- src/app/Components/Footer/Footer.tsx
+++ src/app/Components/Footer/Footer.tsx
@@ -5,16 +5,18 @@ import Social from "../Social/Social";
 const Footer = () => {
   return (
     <div className={styles.footer}>
-      <div>
-        <Logo />
-        <Social className={styles.social} />
+      <div className={styles.wrapper}>
+        <div>
+          <Logo />
+          <Social className={styles.social} />
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
+          minus corrupti sint, illum incidunt dolorem quaerat sunt tenetur,
+          perspiciatis enim alias porro consectetur ducimus voluptate in fugiat
+          vel, soluta modi!
+        </p>
       </div>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
-        minus corrupti sint, illum incidunt dolorem quaerat sunt tenetur,
-        perspiciatis enim alias porro consectetur ducimus voluptate in fugiat
-        vel, soluta modi!
-      </p>
     </div>
   );
 };
