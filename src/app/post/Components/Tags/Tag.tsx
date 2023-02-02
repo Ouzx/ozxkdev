e9d99fa123b6715fe1c4ec85958eb195753491e9
Ouzx <oguzhankandakoglu@gmail.com>
2023-02-02 19:55:12 +0300

    tags hovering

--- src/app/post/Components/Tags/Tag.tsx
+++ src/app/post/Components/Tags/Tag.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Link from "next/link";
 import styles from "./Tag.module.scss";
 const Tag = ({ value }: { value: string }) => {
-  return <div className={styles.tag}>{value}</div>;
+  return (
+    <Link href={"/"}>
+      <div className={styles.tag}>{value}</div>
+    </Link>
+  );
 };
 
 export default Tag;
