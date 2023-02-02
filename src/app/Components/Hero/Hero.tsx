10fa1ac8bc9efc3c6d0968ba6348a8e9abdbdc45
Ouzx <oguzhankandakoglu@gmail.com>
2023-02-02 12:56:33 +0300

    parallax added to hero

--- src/app/Components/Hero/Hero.tsx
+++ src/app/Components/Hero/Hero.tsx
@@ -1,20 +1,24 @@
+"use client";
 import React from "react";
 import Image from "next/image";
 import styles from "./Hero.module.scss";
 
 import hero from "../../../assets/hero.jpg";
 import Searchbar from "../Searchbar/Searchbar";
+import { Parallax } from "react-scroll-parallax";
+
 const Hero = () => {
   return (
-    <div className={styles.hero}>
-      {/* TODO: Make this image parallax also relative to bigger screen sizes */}
-      <Image className={styles.hero__image} alt="hero" src={hero} />
-      <div className={styles.hero__text}>
-        <h1 className={styles.hero__title}>Blog</h1>
-        <p className={styles.hero__subtitle}>A blog about web development</p>
-        <Searchbar />
+    <Parallax translateY={[-300, 180]} className={styles.parallax}>
+      <div className={styles.hero}>
+        <Image className={styles.hero__image} alt="hero" src={hero} />
+        <div className={styles.hero__text}>
+          <h1 className={styles.hero__title}>Blog</h1>
+          <p className={styles.hero__subtitle}>A blog about web development</p>
+          <Searchbar />
+        </div>
       </div>
-    </div>
+    </Parallax>
   );
 };
 
