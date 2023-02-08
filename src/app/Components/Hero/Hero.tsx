cd112bb0391044c0fb2ea4e515468d9f684f862c
Ouzx <oguzhankandakoglu@gmail.com>
2023-02-08 16:17:26 +0300

    parallax removed, hero image changed

--- src/app/Components/Hero/Hero.tsx
+++ src/app/Components/Hero/Hero.tsx
@@ -1,24 +1,19 @@
-"use client";
 import React from "react";
 import Image from "next/image";
 import styles from "./Hero.module.scss";
 
-import hero from "../../../assets/hero.jpg";
+import hero from "../../../assets/hero_x2.jpg";
 import Searchbar from "../Searchbar/Searchbar";
-import { Parallax } from "react-scroll-parallax";
-
 const Hero = () => {
   return (
-    <Parallax translateY={[-300, 180]} className={styles.parallax}>
-      <div className={styles.hero}>
-        <Image className={styles.hero__image} alt="hero" src={hero} />
-        <div className={styles.hero__text}>
-          <h1 className={styles.hero__title}>Blog</h1>
-          <p className={styles.hero__subtitle}>A blog about web development</p>
-          <Searchbar />
-        </div>
+    <div className={styles.hero}>
+      <Image className={styles.hero__image} alt="hero" src={hero} />
+      <div className={styles.hero__text}>
+        <h1 className={styles.hero__title}>Blog</h1>
+        <p className={styles.hero__subtitle}>A blog about web development</p>
+        <Searchbar />
       </div>
-    </Parallax>
+    </div>
   );
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,17 @@
-"use client";
 import Hero from "./Components/Hero/Hero";
 import Pool from "@/Components/Pool/Pool";
 import PostList from "@/Components/PostList/PostList";
 import styles from "./page.module.scss";
-import Contact from "./Components/Contact/Contact";
-import { ParallaxProvider } from "react-scroll-parallax";
 
 export default function Home() {
   return (
-    <ParallaxProvider>
-      <main className={styles.main}>
-        <Hero />
-        <div className={styles.content}>
-          <Pool />
-          <PostList />
-          {/* <Contact /> */}
-        </div>
-      </main>
-    </ParallaxProvider>
+    <main className={styles.main}>
+      <Hero />
+      <div className={styles.content}>
+        <Pool />
+        <PostList />
+        {/* <Contact /> */}
+      </div>
+    </main>
   );
 }
