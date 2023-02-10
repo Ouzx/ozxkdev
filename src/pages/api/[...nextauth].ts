af06dc58490abe38831028315d62490c9f543ec4
Ouzx <oguzhankandakoglu@gmail.com>
2023-02-10 22:44:07 +0300

    next auth api

---
+++ src/pages/api/[...nextauth].ts
@@ -0,0 +1,62 @@
+import CredentialsProvider from "next-auth/providers/credentials";
+import NextAuth from "next-auth";
+import type { NextAuthOptions } from "next-auth";
+import { User } from "next-auth";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: {
+          label: "Username",
+          type: "text",
+          placeholder: "jsmith",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+        },
+      },
+      async authorize(credentials, req) {
+        const { username, password } = credentials as any;
+        const res = await fetch("http://localhost:8000/auth/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username,
+            password,
+          }),
+        });
+
+        const user = await res.json();
+
+        console.log({ user });
+
+        if (res.ok && user) {
+          return user;
+        } else return null;
+      },
+    }),
+  ],
+
+  callbacks: {
+    async jwt({ token, user }) {
+      return { ...token, ...user };
+    },
+    async session({ session, token, user }) {
+      // Send properties to the client, like an access_token from a provider.
+      session.user = token;
+
+      return session;
+    },
+  },
+
+  pages: {
+    signIn: "/auth/login",
+  },
+};
+
+export default NextAuth(authOptions);
