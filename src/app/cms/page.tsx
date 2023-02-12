803671fa1deda2c39ddcf4355451220662a1e590
Ouzx <oguzhankandakoglu@gmail.com>
2023-02-13 01:07:44 +0300

    post listing

--- src/app/cms/page.tsx
+++ src/app/cms/page.tsx
@@ -1,7 +1,47 @@
+"use client";
 import React from "react";
 
-const page = () => {
-  return <div>page</div>;
+import useAccessToken from "@/hooks/useAccessToken";
+import usePosts from "@/hooks/usePosts";
+
+const page = ({
+  searchParams,
+}: {
+  searchParams?: { [key: string]: string | string[] | undefined };
+}) => {
+  const [token] = useAccessToken();
+  const [posts] = usePosts((searchParams?.page as string) || "0", token);
+
+  // table list of posts
+  return (
+    <div>
+      <h1>Posts</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Category</th>
+            <th>Tags</th>
+            <th>Shared</th>
+            <th>Created</th>
+            <th>Updated</th>
+          </tr>
+        </thead>
+        <tbody>
+          {posts?.posts.map((post) => (
+            <tr key={post.slug}>
+              <td>{post.title}</td>
+              <td>{post.shortContent}</td>
+              <td>{post.category}</td>
+              <td>{post.tags.join(", ")}</td>
+              <td>{post.shared ? "Yes" : "No"}</td>
+              <td>{post.createdAt}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default page;
