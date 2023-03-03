6d78defe6a48ef27dfde318a2d05419ce2cd33dd
Ouzx <oguzhankandakoglu@gmail.com>
2023-03-03 22:23:49 +0300

    search fetching moved into component

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,10 +1,18 @@
-import { search } from "@/lib/general";
-import React, { use } from "react";
+import React from "react";
 import PostList from "../Components/Post/PostList/PostList";
 import SearchBar from "../Components/Searchbar/Searchbar";
 import styles from "./page.module.scss";
+import { Posts } from "@/types/Post";
 
-const page = ({
+const search = async (query: string, pageIndex: number): Promise<Posts> => {
+  return await fetch(process.env.API + `/search/${query}/${pageIndex}`)
+    .then((res) => res.json())
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const page = async ({
   searchParams,
 }: {
   searchParams?: { [key: string]: string | string[] | undefined };
@@ -22,7 +30,7 @@ const page = ({
   if (Array.isArray(_searchTerm)) _searchTerm = _searchTerm[0];
   if (Array.isArray(_pageIndex)) _pageIndex = _pageIndex[0];
 
-  const postList = use(search(_searchTerm, +_pageIndex));
+  const postList = await search(_searchTerm, +_pageIndex);
 
   if (!postList || !postList.posts || postList.posts.length === 0) {
     return notFound();
