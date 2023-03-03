70b1a19dd4d2c769ced4086e94b566e7b784b88e
Ouzx <oguzhankandakoglu@gmail.com>
2023-03-03 21:59:19 +0300

    fetch categories moved into component

--- src/app/Components/Pool/Pool.tsx
+++ src/app/Components/Pool/Pool.tsx
@@ -1,15 +1,25 @@
-import React, { use } from "react";
+import React from "react";
 import styles from "./Pool.module.scss";
 import Item from "./Item";
-import { categories } from "@/lib/general";
 
-const Pool = ({
+const getCategories = async () => {
+  return await fetch(process.env.API + "/categories")
+    .then((res) => res.json())
+    .catch((err) => {
+      console.log(err);
+      return [""];
+    });
+};
+
+const Pool = async ({
   selected,
 }: {
   selected: string | null | undefined | string[];
 }) => {
   const selection = selected || "All";
-  const data = ["All", ...use(categories())];
+  const categories = await getCategories();
+  const data = ["All", ...categories];
+
   let items = data.map((item) => (
     <Item title={item} path={item} key={item} selected={selection === item} />
   ));
