08f918685f931c3787c916016cfb3df5438fbccb
Ouzx <oguzhankandakoglu@gmail.com>
2023-03-10 19:04:06 +0300

    delete logic update

--- src/app/cms/Components/DeleteButton/DeleteButton.tsx
+++ src/app/cms/Components/DeleteButton/DeleteButton.tsx
@@ -24,6 +24,7 @@ const DeleteButton = ({ postId, postTitle }: Props) => {
   };
 
   const onYes = async () => {
+    if (isMutating) return;
     trigger({ postId, token });
     setShowModal(false);
 
