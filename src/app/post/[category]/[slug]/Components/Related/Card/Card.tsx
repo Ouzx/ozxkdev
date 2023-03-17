0c51aabca329c3e7954d2e1dfb82b84a8544b258
Ouzx <oguzhankandakoglu@gmail.com>
2023-03-17 10:23:07 +0300

    related post update

--- src/app/post/[category]/[slug]/Components/Related/Card/Card.tsx
+++ src/app/post/[category]/[slug]/Components/Related/Card/Card.tsx
@@ -3,7 +3,7 @@ import styles from "./Card.module.scss";
 import Image from "next/image";
 import Link from "next/link";
 import { AiOutlineTag } from "react-icons/ai";
-import card from "@/assets/card.jpg";
+
 import { iPost } from "@/types/Post";
 
 const Card = ({ post }: { post: iPost }) => {
@@ -13,7 +13,13 @@ const Card = ({ post }: { post: iPost }) => {
         <Link href={`/post/${post.category}/${post.slug}`}>
           <div className={styles.card}>
             <div className={styles.img_wrapper}>
-              <Image className={styles.img} src={card} alt="card" />
+              <Image
+                className={styles.img}
+                width={300}
+                height={200}
+                src={post.thumbnail}
+                alt={post.title}
+              />
             </div>
             <div className={styles.info_wrapper}>
               <h3>{post.title}</h3>
