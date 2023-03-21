7b72535528b4f4a26d863eeec486b9d7b87ddd86
Ouzx <oguzhankandakoglu@gmail.com>
2023-03-21 22:17:12 +0300

    error and 404 styling update

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,7 +1,10 @@
 "use client"; // Error components must be Client components
 
 import { useEffect } from "react";
-
+import Image from "next/image";
+import Link from "next/link";
+import errorPic from "@/assets/error.jpg";
+import styles from "./error.module.scss";
 export default function Error({
   error,
   reset,
@@ -15,16 +18,13 @@ export default function Error({
   }, [error]);
 
   return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        Try again
-      </button>
+    <div className={styles.container}>
+      <Image className={styles.image} src={errorPic} alt="error" />
+      <Link href={"/"}>
+        <button className={styles.button}>
+          <span>Go back to home</span>
+        </button>
+      </Link>
     </div>
   );
 }
--- src/app/not-Found.tsx
+++ src/app/not-Found.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import errorPic from "@/assets/error.jpg";
+import styles from "./not-Found.module.scss";
 
 const notFound = () => {
-  return <div>notFound</div>;
+  return (
+    <div className={styles.container}>
+      <Image className={styles.image} src={errorPic} alt="error" />
+      <Link href={"/"}>
+        <button className={styles.button}>
+          <span>Go back to home</span>
+        </button>
+      </Link>
+    </div>
+  );
 };
 
 export default notFound;
