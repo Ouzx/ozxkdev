95c2d2e98297cd60c469b8ef650ece13cb152786
Ouzx <oguzhankandakoglu@gmail.com>
2023-03-21 22:44:27 +0300

    not found naming fixed

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import errorPic from "@/assets/error.jpg";
-import styles from "./not-Found.module.scss";
+import styles from "./not-found.module.scss";
 
 const NotFound = () => {
   console.log("not found");
