b41a0022caa5d6c757f3ee529a15ef6342735b91
Ouzx <oguzhankandakoglu@gmail.com>
2023-03-23 01:36:30 +0300

    project card update

--- src/app/portfolio/Components/Projects/ProjectButton/ProjectButton.tsx
+++ src/app/portfolio/Components/Projects/ProjectButton/ProjectButton.tsx
@@ -1,7 +1,15 @@
 import React from "react";
-
+import { AiFillGithub } from "react-icons/ai";
+import styles from "./ProjectButton.module.scss";
 const ProjectButton = ({ link, title }: { link: string; title: string }) => {
-  return <div>ProjectButton</div>;
+  return (
+    <a href={link} rel="noopener noreferrer" target="_blank">
+      <button className={styles.projectButton}>
+        <AiFillGithub />
+        <span>{title}</span>
+      </button>
+    </a>
+  );
 };
 
 export default ProjectButton;
