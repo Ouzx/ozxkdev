d9d0fc1d64605788f2c2ec6e61722a7b743a2ca1
Ouzx <oguzhankandakoglu@gmail.com>
2023-03-22 22:18:26 +0300

    HR styling update

--- src/app/portfolio/Components/HR/HR.tsx
+++ src/app/portfolio/Components/HR/HR.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./HR.module.scss";
-const HR = () => {
-  return <div className={styles.seperator}></div>;
+const HR = ({ half }: { half?: boolean }) => {
+  return <div className={`${styles.seperator} ${half && styles.half}`}></div>;
 };
 
 export default HR;
