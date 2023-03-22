efb808a43c4dfc15d3c9aa90538910db42de1124
Ouzx <oguzhankandakoglu@gmail.com>
2023-03-22 14:49:04 +0300

    search page v2

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,41 +1,51 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
+import { useSearchParams } from "next/navigation";
 import PostList from "../Components/Post/PostList/PostList";
 import SearchBar from "../Components/Searchbar/Searchbar";
 import styles from "./page.module.scss";
 import { Posts } from "@/types/Post";
-import { notFound } from "next/navigation";
 
 const search = async (query: string, pageIndex: number): Promise<Posts> => {
-  return await fetch(process.env.API + `/search/${query}/${pageIndex}`)
+  return await fetch(
+    process.env.NEXT_PUBLIC_CMS_API + `/general/search/${query}/${pageIndex}`
+  )
     .then((res) => res.json())
     .catch((err) => {
       console.log(err);
     });
 };
 
-const page = async ({
-  searchParams,
-}: {
-  searchParams?: { [key: string]: string | string[] | undefined };
-}) => {
-  const searchTerm = searchParams?.term;
-  const pageIndex = searchParams?.page;
+const page = () => {
+  const [searchTerm, setSearchTerm] = useState<string | null>(null);
+  const [pageIndex, setPageIndex] = useState<number>(1);
+  const [postList, setPostList] = useState<Posts | null>(null);
 
-  let _searchTerm = searchTerm ? searchTerm : null;
-  let _pageIndex = pageIndex ? pageIndex : 1;
+  const searchParams = useSearchParams();
+  const _searchTerm = searchParams.get("term");
+  const _pageIndex = searchParams.get("page");
 
-  if (Array.isArray(_searchTerm)) _searchTerm = _searchTerm[0];
-  if (Array.isArray(_pageIndex)) _pageIndex = _pageIndex[0];
-  let postList: Posts | null = null;
-  if (_searchTerm) postList = await search(_searchTerm!, +_pageIndex);
+  useEffect(() => {
+    setSearchTerm(_searchTerm);
+    const _page = parseInt(_pageIndex || "1");
+    setPageIndex(_page);
+  }, [searchParams]);
+
+  useEffect(() => {
+    if (searchTerm && pageIndex) {
+      search(searchTerm, pageIndex).then((res) => {
+        setPostList(res);
+      });
+    }
+  }, [searchTerm, pageIndex]);
 
   return (
     <div className={styles.search}>
       <h1>
         {postList
           ? postList.totalItems >= 0
-            ? `Search results for "${_searchTerm}"`
-            : `No results found for "${_searchTerm}"`
+            ? `Search results for "${searchTerm}"`
+            : `No results found for "${searchTerm}"`
           : "Search Something"}
       </h1>
       <SearchBar />
