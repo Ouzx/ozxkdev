dffa5908f18f9590cd1eecb42103bad6b5c1289c
Ouzx <oguzhankandakoglu@gmail.com>
2023-03-25 14:04:38 +0300

    share icon fix

--- src/app/post/[category]/[slug]/Components/Share/Share.tsx
+++ src/app/post/[category]/[slug]/Components/Share/Share.tsx
@@ -5,7 +5,12 @@ import {
   TwitterShareButton,
   LinkedinShareButton,
 } from "react-share";
-import { FacebookIcon, TwitterIcon, LinkedinIcon } from "react-share";
+// import { FacebookIcon, TwitterIcon, LinkedinIcon } from "react-share";
+import {
+  AiFillFacebook,
+  AiFillTwitterCircle,
+  AiFillLinkedin,
+} from "react-icons/ai";
 
 import styles from "./Share.module.scss";
 const Share = ({ url, title }: { url: string; title: string }) => {
@@ -14,14 +19,16 @@ const Share = ({ url, title }: { url: string; title: string }) => {
       <h2 className={styles.title}>Share</h2>
       <div className={styles.share}>
         <FacebookShareButton url={url} quote={title + "\n"} hashtag="#ozxkdev">
-          <FacebookIcon size={32} round />
+          {/* <FacebookIcon size={32} round /> */}
+          <AiFillFacebook size={32} className={styles.icon} />
         </FacebookShareButton>
         <TwitterShareButton
           url={url}
           title={title + "\n"}
           hashtags={["ozxkdev"]}
         >
-          <TwitterIcon size={32} round />
+          {/* <TwitterIcon size={32} round /> */}
+          <AiFillTwitterCircle size={32} className={styles.icon} />
         </TwitterShareButton>
         <LinkedinShareButton
           url={url}
@@ -29,7 +36,8 @@ const Share = ({ url, title }: { url: string; title: string }) => {
           summary={title}
           source={url}
         >
-          <LinkedinIcon size={32} round />
+          {/* <LinkedinIcon size={32} round /> */}
+          <AiFillLinkedin size={32} className={styles.icon} />
         </LinkedinShareButton>
       </div>
     </div>
