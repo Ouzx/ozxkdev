e7f65eacfcefa98e26d5fe460ad47889450dc616
Ouzx <oguzhankandakoglu@gmail.com>
2023-04-01 21:16:41 +0300

    search page update

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -6,24 +6,53 @@ import SearchBar from "../Components/Searchbar/Searchbar";
 import styles from "./page.module.scss";
 import { Posts } from "@/types/Post";
 
-const search = async (query: string, pageIndex: number): Promise<Posts> => {
-  return await fetch(
-    process.env.NEXT_PUBLIC_CMS_API + `/general/search/${query}/${pageIndex}`
-  )
-    .then((res) => res.json())
-    .catch((err) => {
-      console.log(err);
-    });
+const search = (query: string, pageIndex: number) => {
+  const [success, setSuccess] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+  const [postData, setPostData] = useState<Posts | undefined>(undefined);
+
+  const fetchPost = () => {
+    setLoading(true);
+
+    fetch(
+      process.env.NEXT_PUBLIC_CMS_API + `/general/search/${query}/${pageIndex}`,
+      {
+        cache: "no-cache",
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.totalItems === 0) {
+          setError("No results found");
+        } else {
+          setSuccess(true);
+          setPostData(data);
+        }
+      })
+      .catch((err) => {
+        setError(err.message);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  return { success, loading, error, postData, fetchPost };
 };
 
 const page = () => {
   const [searchTerm, setSearchTerm] = useState<string | null>(null);
   const [pageIndex, setPageIndex] = useState<number>(1);
-  const [postList, setPostList] = useState<Posts | null>(null);
 
   const searchParams = useSearchParams();
   const _searchTerm = searchParams.get("term");
   const _pageIndex = searchParams.get("page");
+
+  const { success, loading, error, postData, fetchPost } = search(
+    searchTerm!,
+    pageIndex
+  );
 
   useEffect(() => {
     setSearchTerm(_searchTerm);
@@ -33,27 +62,28 @@ const page = () => {
 
   useEffect(() => {
     if (searchTerm && pageIndex) {
-      search(searchTerm, pageIndex).then((res) => {
-        setPostList(res);
-      });
+      fetchPost();
     }
   }, [searchTerm, pageIndex]);
 
   return (
     <div className={styles.search}>
-      <h1>
-        {postList
-          ? postList.totalItems >= 0
-            ? `Search results for "${searchTerm}"`
-            : `No results found for "${searchTerm}"`
-          : "Search Something"}
-      </h1>
+      {error ? (
+        <>
+          <h1>No results found for "{searchTerm}"</h1>
+          <h2>Try searching for something else</h2>
+        </>
+      ) : loading ? (
+        <h1>Searching for "{searchTerm}"...</h1>
+      ) : success ? (
+        <h1>Search Results for "{searchTerm}"</h1>
+      ) : searchTerm ? (
+        <h1>Searching for "{searchTerm}"...</h1>
+      ) : (
+        <h1>Search Something</h1>
+      )}
       <SearchBar />
-      <React.Suspense>
-        {postList && postList.totalItems > 0 && (
-          <PostList postList={postList} />
-        )}
-      </React.Suspense>
+      <PostList postList={postData as Posts} />
     </div>
   );
 };
