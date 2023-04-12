f3cb7fddbdb76e51c74858c03c0af43208ba269c
Ouzx <oguzhankandakoglu@gmail.com>
2023-04-12 13:27:21 +0300

    project button update

--- src/app/portfolio/Components/Projects/ProjectButton/ProjectButton.tsx
+++ src/app/portfolio/Components/Projects/ProjectButton/ProjectButton.tsx
@@ -4,10 +4,15 @@ import styles from "./ProjectButton.module.scss";
 const ProjectButton = ({ link, title }: { link: string; title: string }) => {
   return (
     <a href={link} rel="noopener noreferrer" target="_blank">
-      <button className={styles.projectButton}>
-        <AiFillGithub />
-        <span>{title}</span>
-      </button>
+      <div className={styles.btn}>
+        <button className={styles.projectButton}>
+          <AiFillGithub />
+          <span>{title}</span>
+        </button>
+      </div>
+      <div className={styles.link}>
+        <span>{link}</span>
+      </div>
     </a>
   );
 };
