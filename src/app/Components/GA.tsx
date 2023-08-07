258693da3d79bef28ac7771d266dfc9a6d1efe8e
Ouzx <oguzhankandakoglu@gmail.com>
2023-08-07 22:00:08 +0300

    Google analytics implemented

---
+++ src/app/Components/GA.tsx
@@ -0,0 +1,8 @@
+"use client";
+import React from "react";
+import { GoogleAnalytics } from "nextjs-google-analytics";
+const GA = () => {
+	return <GoogleAnalytics trackPageViews />;
+};
+
+export default GA;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,50 +1,52 @@
 import { Metadata } from "next";
+import GA from "./Components/GA";
 import Hero from "./Components/Hero/Hero";
 import MainContent from "./Components/MainContent/MainContent";
 import styles from "./page.module.scss";
 
 export async function generateMetadata(): Promise<Metadata> {
-  return {
-    title: "ozxk's Code Chronicles: A Dev Log",
-    description: "Home",
-    openGraph: {
-      title: "Home",
-      description:
-        "Get an inside look at the world of software development through ozxk's eyes",
-      url: `${process.env.NEXT_PUBLIC_URL}`,
-      images: [
-        {
-          url: `${process.env.NEXT_PUBLIC_URL}/og-image.png`,
-          width: 1200,
-          height: 630,
-          alt: "og-image",
-        },
-      ],
-    },
-    twitter: {
-      card: "summary_large_image",
-      site: "@Ozx_K",
-      description:
-        "Get an inside look at the world of software development through ozxk's eyes",
-      title: "Ozxk Dev Blog",
-      images: [
-        {
-          url: `${process.env.NEXT_PUBLIC_URL}/og-image.png`,
-          width: 1200,
-          height: 630,
-          alt: "og-image",
-        },
-      ],
-    },
-    themeColor: "#47c9e5",
-  };
+	return {
+		title: "ozxk's Code Chronicles: A Dev Log",
+		description: "Home",
+		openGraph: {
+			title: "Home",
+			description:
+				"Get an inside look at the world of software development through ozxk's eyes",
+			url: `${process.env.NEXT_PUBLIC_URL}`,
+			images: [
+				{
+					url: `${process.env.NEXT_PUBLIC_URL}/og-image.png`,
+					width: 1200,
+					height: 630,
+					alt: "og-image",
+				},
+			],
+		},
+		twitter: {
+			card: "summary_large_image",
+			site: "@Ozx_K",
+			description:
+				"Get an inside look at the world of software development through ozxk's eyes",
+			title: "Ozxk Dev Blog",
+			images: [
+				{
+					url: `${process.env.NEXT_PUBLIC_URL}/og-image.png`,
+					width: 1200,
+					height: 630,
+					alt: "og-image",
+				},
+			],
+		},
+		themeColor: "#47c9e5",
+	};
 }
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <Hero />
-      <MainContent />
-    </main>
-  );
+	return (
+		<main className={styles.main}>
+			<Hero />
+			<MainContent />
+			<GA />
+		</main>
+	);
 }
