84891398fcf3b286e609e9f72f3243ab1fc24b8f
Ouzx <oguzhankandakoglu@gmail.com>
2023-08-09 04:44:52 +0300

    codeblock info section added

--- src/app/post/[category]/[slug]/Components/Content/Components/Renderers/Code/OzCodeBlock.tsx
+++ src/app/post/[category]/[slug]/Components/Content/Components/Renderers/Code/OzCodeBlock.tsx
@@ -4,100 +4,113 @@ import type { CSSProperties } from "react";
 import { PrismAsync as SyntaxHighlighter } from "react-syntax-highlighter";
 import { atomDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 
+import styles from "./OzCodeBlock.module.scss";
+
 const defaultStyle: { [key: string]: CSSProperties } = {
-  container: {
-    // dropShadow
-    boxShadow: "0 0 0.5rem 0.5rem rgba(0, 0, 0, 0.1)",
-    borderRadius: "1rem",
-    margin: "2rem 0",
-  },
-  code: {
-    borderRadius: "1rem",
-  },
+	container: {
+		// dropShadow
+		boxShadow: "0 0 0.5rem 0.5rem rgba(0, 0, 0, 0.1)",
+		margin: "2rem 0",
+		borderRadius: "1rem",
+	},
+	code: {
+		borderRadius: "1rem",
+		borderTopLeftRadius: "0",
+		borderTopRightRadius: "0",
+	},
 };
 
 type CodeBoxOutputData = {
-  code: string;
+	code: string;
 };
 
 type CodeBoxOutputClassNames = {
-  container?: string;
-  code?: string;
+	container?: string;
+	code?: string;
 };
 
 type CodeBoxOutputStyles = {
-  container?: CSSProperties;
-  code?: CSSProperties;
+	container?: CSSProperties;
+	code?: CSSProperties;
 };
 type CodeBoxOutputProps = {
-  data: CodeBoxOutputData;
-  style?: CodeBoxOutputStyles;
-  classNames?: CodeBoxOutputClassNames;
-  config?: ErrConfig;
+	data: CodeBoxOutputData;
+	style?: CodeBoxOutputStyles;
+	classNames?: CodeBoxOutputClassNames;
+	config?: ErrConfig;
 };
 
 type ErrConfig = {
-  disableDefaultStyle: boolean;
+	disableDefaultStyle: boolean;
 };
 
 const OzCodeBlock = ({
-  data,
-  style,
-  classNames,
-  config,
+	data,
+	style,
+	classNames,
+	config,
 }: CodeBoxOutputProps): JSX.Element => {
-  if (!style || typeof style !== "object") style = {};
-  if (!config || typeof config !== "object")
-    config = { disableDefaultStyle: false };
-  if (!classNames || typeof classNames !== "object") classNames = {};
+	if (!style || typeof style !== "object") style = {};
+	if (!config || typeof config !== "object")
+		config = { disableDefaultStyle: false };
+	if (!classNames || typeof classNames !== "object") classNames = {};
 
-  if (!style.container || typeof style.container !== "object")
-    style.container = {};
-  if (!classNames.container || typeof classNames.container !== "string")
-    classNames.container = "";
+	if (!style.container || typeof style.container !== "object")
+		style.container = {};
+	if (!classNames.container || typeof classNames.container !== "string")
+		classNames.container = "";
 
-  if (!style.code || typeof style.code !== "object") style.code = {};
-  if (!classNames.code || typeof classNames.code !== "string")
-    classNames.code = "";
+	if (!style.code || typeof style.code !== "object") style.code = {};
+	if (!classNames.code || typeof classNames.code !== "string")
+		classNames.code = "";
 
-  if (!data || !data.code || typeof data.code != "string") return <></>;
-  const containerStyle = config.disableDefaultStyle
-    ? style.container
-    : { ...defaultStyle.container, ...style.container };
-  const codeStyle = config.disableDefaultStyle
-    ? style.code
-    : { ...defaultStyle.code, ...style.code };
+	if (!data || !data.code || typeof data.code != "string") return <></>;
+	const containerStyle = config.disableDefaultStyle
+		? style.container
+		: { ...defaultStyle.container, ...style.container };
+	const codeStyle = config.disableDefaultStyle
+		? style.code
+		: { ...defaultStyle.code, ...style.code };
 
-  let content = null;
+	let content = null;
 
-  if (typeof data === "string") content = data;
-  else if (
-    typeof data === "object" &&
-    data.code &&
-    typeof data.code === "string"
-  )
-    content = data.code;
+	if (typeof data === "string") content = data;
+	else if (
+		typeof data === "object" &&
+		data.code &&
+		typeof data.code === "string"
+	)
+		content = data.code;
 
-  const [lang, ...code] = data.code.split("\n");
-  let language = "javascript";
-  if (lang.startsWith("#")) {
-    language = lang.replace("#", "");
-    content = code.join("\n");
-  }
-  return content ? (
-    <div style={containerStyle}>
-      <SyntaxHighlighter
-        showLineNumbers
-        language={language || "javascript"}
-        style={atomDark}
-        customStyle={codeStyle}
-      >
-        {content}
-      </SyntaxHighlighter>
-    </div>
-  ) : (
-    <></>
-  );
+	let [info, ...code] = data.code.split("\n");
+	let language = "javascript";
+	let path = "";
+	if (info.startsWith("#")) {
+		[language, path] = info.split("#").slice(1);
+		content = code.join("\n");
+	}
+	return content ? (
+		<div
+			style={containerStyle}
+			className={styles.container}
+		>
+			<div className={styles.top}>
+				<span className={styles.path}>{path}</span>
+				<span className={styles.lang}>{language}</span>
+			</div>
+
+			<SyntaxHighlighter
+				showLineNumbers
+				language={language || "javascript"}
+				style={atomDark}
+				customStyle={codeStyle}
+			>
+				{content}
+			</SyntaxHighlighter>
+		</div>
+	) : (
+		<></>
+	);
 };
 
 export default OzCodeBlock;
--- src/app/post/[category]/[slug]/Components/Content/Content.tsx
+++ src/app/post/[category]/[slug]/Components/Content/Content.tsx
@@ -3,127 +3,133 @@ import React from "react";
 import dynamic from "next/dynamic";
 import OzCodeBlock from "./Components/Renderers/Code/OzCodeBlock";
 const Output = dynamic(
-  async () => (await import("editorjs-react-renderer")).default,
-  { ssr: false }
+	async () => (await import("editorjs-react-renderer")).default,
+	{ ssr: false }
 );
 
 const style = {
-  paragraph: {
-    // marginTop: "1rem",
-    lineHeight: "1.6em",
-    letterSpacing: "0.04em",
-    marginBottom: "1rem",
-  },
-  warning: {
-    container: {
-      backgroundColor: "#fff3cd",
-      border: "1px solid #ffeeba",
-      borderRadius: "0.5rem",
-      padding: "1rem",
-      boxShadow: "0 0 0.5rem 0.1rem rgba(0, 0, 0, 0.2)",
-      alignItems: "center",
-    },
-    icon: {
-      marginRight: "0.5rem",
-    },
-    title: {
-      fontSize: "1.2rem",
-      fontWeight: "bold",
-    },
-    message: {
-      lineHeight: "1.6em",
-      letterSpacing: "0.04em",
-      fontWeight: "500",
-    },
-  },
-  image: {
-    img: {
-      gridArea: "stack",
-    },
-    figure: {
-      display: "grid",
-      gridTemplateAreas: '"stack"',
-    },
-    figcaption: {
-      gridArea: "stack",
-    },
-  },
-  list: {
-    //   container: {...},
-    listItem: {
-      lineHeight: "1.6em",
-      letterSpacing: "0.04em",
-    },
-  },
-  checklist: {
-    container: {
-      marginBottom: "2rem",
-    },
-    item: {
-      lineHeight: "1.6em",
-      letterSpacing: "0.04em",
-      alignItems: "flex-start",
-      marginBottom: "1rem",
-    },
-    checkbox: {
-      width: "1.2rem",
-      height: "1.2rem",
-      flexShrink: 0,
-    },
-    label: {
-      lineHeight: "1.6em",
-      letterSpacing: "0.04em",
-    },
-  },
-  table: {
-    table: {
-      lineHeight: "1.6em",
-      letterSpacing: "0.04em",
-    },
-    //   tr: {...},
-    //   th: {...},
-    //   td: {...},
-  },
-  quote: {
-    container: {
-      justifyContent: "flex-start",
-      margin: "1rem 0",
-      border: "1px solid #f2f2f2",
-      borderRadius: "0.5rem",
-      padding: "1rem",
-      // drop shadow
-      boxShadow: "0 0 0.5rem 0.1rem rgba(0, 0, 0, 0.2)",
-    },
-    content: {},
-    author: {},
-    // message: {...}
-  },
+	paragraph: {
+		// marginTop: "1rem",
+		lineHeight: "1.6em",
+		letterSpacing: "0.04em",
+		marginBottom: "1rem",
+	},
+	warning: {
+		container: {
+			backgroundColor: "#fff3cd",
+			border: "1px solid #ffeeba",
+			borderRadius: "0.5rem",
+			padding: "1rem",
+			boxShadow: "0 0 0.5rem 0.1rem rgba(0, 0, 0, 0.2)",
+			alignItems: "center",
+		},
+		icon: {
+			marginRight: "0.5rem",
+		},
+		title: {
+			fontSize: "1.2rem",
+			fontWeight: "bold",
+		},
+		message: {
+			lineHeight: "1.6em",
+			letterSpacing: "0.04em",
+			fontWeight: "500",
+		},
+	},
+	image: {
+		img: {
+			gridArea: "stack",
+		},
+		figure: {
+			display: "grid",
+			gridTemplateAreas: '"stack"',
+		},
+		figcaption: {
+			gridArea: "stack",
+		},
+	},
+	list: {
+		//   container: {...},
+		listItem: {
+			lineHeight: "1.6em",
+			letterSpacing: "0.04em",
+		},
+	},
+	checklist: {
+		container: {
+			marginBottom: "2rem",
+		},
+		item: {
+			lineHeight: "1.6em",
+			letterSpacing: "0.04em",
+			alignItems: "flex-start",
+			marginBottom: "1rem",
+		},
+		checkbox: {
+			width: "1.2rem",
+			height: "1.2rem",
+			flexShrink: 0,
+		},
+		label: {
+			lineHeight: "1.6em",
+			letterSpacing: "0.04em",
+		},
+	},
+	table: {
+		table: {
+			lineHeight: "1.6em",
+			letterSpacing: "0.04em",
+		},
+		//   tr: {...},
+		//   th: {...},
+		//   td: {...},
+	},
+	quote: {
+		container: {
+			justifyContent: "flex-start",
+			margin: "1rem 0",
+			border: "1px solid #f2f2f2",
+			borderRadius: "0.5rem",
+			padding: "1rem",
+			// drop shadow
+			boxShadow: "0 0 0.5rem 0.1rem rgba(0, 0, 0, 0.2)",
+		},
+		content: {},
+		author: {},
+		// message: {...}
+	},
 
-  delimiter: {
-    container: {
-      width: "80%",
-      height: "1px",
-      borderRadius: "999px",
-      backgroundColor: "#f2f2f2",
-      margin: "1rem auto",
-    },
-    svg: {
-      display: "none",
-    },
-    path: {
-      display: "none",
-    },
-  },
+	delimiter: {
+		container: {
+			width: "80%",
+			height: "1px",
+			borderRadius: "999px",
+			backgroundColor: "#f2f2f2",
+			margin: "1rem auto",
+		},
+		svg: {
+			display: "none",
+		},
+		path: {
+			display: "none",
+		},
+	},
 };
 
 const renderers = {
-  asd: OzCodeBlock,
+	asd: OzCodeBlock,
 };
 
 const Content = ({ content }: { content: string }) => {
-  const data = JSON.parse(content);
+	const data = JSON.parse(content);
 
-  return <Output renderers={renderers} data={data} style={style} />;
+	return (
+		<Output
+			renderers={renderers}
+			data={data}
+			style={style}
+		/>
+	);
 };
 
 export default Content;
