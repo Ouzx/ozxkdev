3ed99e065a4eb8ce72de1a9d07fe78b2c4820347
Ouzx <oguzhankandakoglu@gmail.com>
2023-08-10 21:32:20 +0300

    resume update

--- src/app/resume/Components/Badges/Badges.tsx
+++ src/app/resume/Components/Badges/Badges.tsx
@@ -2,13 +2,25 @@ import React from "react";
 import Section from "./Section";
 
 const Badges = () => {
-  return (
-    <>
-      <Section title={"Frontend"} start={0} end={10} />
-      <Section title={"Backend"} start={10} end={20} />
-      <Section title={"Others"} start={20} end={40} />
-    </>
-  );
+	return (
+		<>
+			<Section
+				title={"Frontend"}
+				start={0}
+				end={11}
+			/>
+			<Section
+				title={"Backend"}
+				start={11}
+				end={20}
+			/>
+			<Section
+				title={"Others"}
+				start={20}
+				end={41}
+			/>
+		</>
+	);
 };
 
 export default Badges;
