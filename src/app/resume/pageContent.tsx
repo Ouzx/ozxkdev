517e1ba94c962dde82e6c307833ebf141891402f
Ouzx <oguzhankandakoglu@gmail.com>
2023-09-24 14:37:01 +0300

    page break Update

--- src/app/resume/pageContent.tsx
+++ src/app/resume/pageContent.tsx
@@ -20,57 +20,65 @@ import Certifications from "./Components/Fixed/Certifications";
 import Langs from "./Components/Langs/Langs";
 import Projects from "./Components/Projects/Projects";
 
+import "./print.css";
+
 const pageContent = () => {
-  return (
-    <DataProvider>
-      <div className={styles.container}>
-        <Image src={heroBG} alt="" className={styles.bg} />
-        <div className={styles.content}>
-          <div className={styles.side}>
-            <div className={styles.row}>
-              <Profile />
-            </div>
+	return (
+		<DataProvider>
+			<div
+				className={styles.container}
+				id="pageP"
+			>
+				<Image
+					src={heroBG}
+					alt=""
+					className={styles.bg}
+				/>
+				<div className={styles.content}>
+					<div className={styles.side}>
+						<div className={styles.row}>
+							<Profile />
+						</div>
 
-            <div className={styles.row}>
-              <Contact />
-            </div>
+						<div className={styles.row}>
+							<Contact />
+						</div>
 
-            <div className={`${styles.row} `}>
-              <Langs />
-            </div>
+						<div className={`${styles.row} `}>
+							<Langs />
+						</div>
 
-            <div
-              className={`${styles.row} ${styles.page_break}`}
-              style={{ marginTop: "2.2rem" }}
-            >
-              <Badges />
-            </div>
+						<div
+							className={`${styles.row} ${styles.page_break}`}
+							style={{ marginTop: "2.2rem" }}
+						>
+							<Badges />
+						</div>
 
-            <div className={`${styles.row} ${styles.buttons}`}>
-              <ExButtons />
-            </div>
-          </div>
+						<div className={`${styles.row} ${styles.buttons}`}>
+							<ExButtons />
+						</div>
+					</div>
 
-          <div className={styles.main}>
-            <Hello />
-            <HR />
-            <Experience />
-            <HR />
-            <div className={styles.printSpacer}></div>
-            <Education />
-            <HR />
-            <div className={styles.printSpacer}></div>
-            <Certifications />
-            <HR />
-            {/* <div className={styles.printSpacer10}></div>*/}
-
-            <div className={styles.page_break}></div>
-            <Projects />
-          </div>
-        </div>
-      </div>
-    </DataProvider>
-  );
+					<div className={styles.main}>
+						<Hello />
+						<HR />
+						<Experience />
+						<HR />
+						<Education />
+						<HR />
+						{/* <div className={styles.printSpacer}></div> */}
+						<Certifications />
+						<HR />
+						{/* <div className={styles.printSpacer10}></div>*/}
+						{/* <div className={styles.printSpacer}></div> */}
+						<div className={styles.page_break}></div>
+						<Projects />
+					</div>
+				</div>
+			</div>
+		</DataProvider>
+	);
 };
 
 export default pageContent;
