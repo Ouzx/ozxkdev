473837a39185c302d44e06aaa799e76bb23e4b73
Ouzx <oguzhankandakoglu@gmail.com>
2023-09-24 14:36:45 +0300

    pretty update

--- src/app/resume/Components/Title/Title.tsx
+++ src/app/resume/Components/Title/Title.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./Title.module.scss";
 const Title = ({ title }: { title: string }) => {
-  return <p className={styles.info}>{title}</p>;
+	return <p className={styles.info}>{title}</p>;
 };
 
 export default Title;
